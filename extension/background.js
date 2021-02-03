0701ac1686b86563853851b1fa168c5b85c066fa
jatin <sandilya.jatin@gmail.com>
2021-02-03 17:57:12 +0530

    v0 after publishing to web store

--- extension/background.js
+++ extension/background.js
@@ -61,6 +61,10 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
         else if (message.name === 'publish') {
             publish();
         }
+        else if (message.name === "login") {
+            console.log("TEST login", message)
+            login();
+        }
     }
     return true;
 });
@@ -73,6 +77,8 @@ chrome.runtime.onInstalled.addListener(function () {
         type: 'normal',
         contexts: ['all']
     });
+    chrome.storage.sync.remove(['mode', 'apiKey', 'guid'], function (Items) {
+    });
 });
 
 chrome.contextMenus.onClicked.addListener(function (event) {
@@ -83,7 +89,7 @@ chrome.contextMenus.onClicked.addListener(function (event) {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, { name: "capture-step" }, function (response) {
                 console.log("capture-step response", response);
-                let step = { screenId: tabs[0].url, view: { "tooltipStyle": { "actionView": { "backgroundColor": "#3d72dc", "display": "flex", "alignItems": "center", "justifyContent": "center", "width": 90, "height": 30, "borderRadius": 4 }, "wrapper": { "borderColor": "#ffffff", "borderWidth": 1, "borderRadius": 5, "zIndex": 4 }, "actionText": { "color": "white" }, "skipActionText": { "fontSize": 16, "color": "rgb(217 217 217)", "fontWeight": "bold" }, "skipAction": { "bottom": 58, "left": 15, "zIndex": 2, "fontSize": 16, "fontWeight": "bold", "position": "absolute" }, "primaryText": { "top": 113.61904999999999, "left": 17.142857, "color": "#747474", "fontSize": 16 }, "secondaryText": { "top": "113.61904999999999px", "left": "17.142857px", "color": "#747474", "font-size": "16px" }, "focussedView": { "top": 143.61905, "left": 17.142857, "width": 115.04762, "height": 115.04762 }, "indexView": { "color": "#747474" }, "tooltip": { "backgroundColor": "#1a237e" } }, "content": { "skipAction": "Skip", "actionText": "Next", "primaryText": "Text describing this view", "secondaryText": "Elaborate text" } }, id: response };
+                let step = { screenId: tabs[0].url, view: { "tooltipStyle": { "actionView": { "backgroundColor": "#3d72dc", "display": "flex", "alignItems": "center", "justifyContent": "center", "max-width": 90, "height": 30, "borderRadius": 4 }, "wrapper": { "borderColor": "#ffffff", "borderWidth": 1, "borderRadius": 5, "zIndex": 4 }, "actionText": { "color": "white" }, "skipActionText": { "fontSize": 16, "color": "rgb(217 217 217)", "fontWeight": "bold" }, "skipAction": { "bottom": 58, "left": 15, "zIndex": 2, "fontSize": 16, "fontWeight": "bold", "position": "absolute" }, "primaryText": { "top": 113.61904999999999, "left": 17.142857, "color": "#747474", "fontSize": 16 }, "secondaryText": { "top": "113.61904999999999px", "left": "17.142857px", "color": "#747474", "font-size": "16px" }, "focussedView": { "top": 143.61905, "left": 17.142857, "width": 115.04762, "height": 115.04762 }, "indexView": { "color": "#747474" }, "tooltip": { "backgroundColor": "#1a237e" } }, "content": { "skipAction": "Skip", "actionText": "Next", "primaryText": "Text describing this view", "secondaryText": "Elaborate text" } }, id: response };
                 let temp = steps[tabs[0].url] || [];
                 steps[tabs[0].url] = temp.concat(step);
                 console.log("Steps as of now:", steps);
@@ -91,39 +97,74 @@ chrome.contextMenus.onClicked.addListener(function (event) {
         });
     }
 })
+
+var login = function () {
+    chrome.identity.getAuthToken({
+        interactive: true
+    }, function (token) {
+        if (chrome.runtime.lastError) {
+            alert(chrome.runtime.lastError.message);
+            return;
+        }
+        var x = new XMLHttpRequest();
+        x.open('GET', 'https://www.googleapis.com/oauth2/v2/userinfo?alt=json&access_token=' + token);
+        x.onload = function () {
+            var userDetails = JSON.parse(x.response);
+            var postLogin = new XMLHttpRequest();
+            postLogin.open('POST', 'https://api.flowdash.ai/login?email=' + userDetails.email);
+            postLogin.onload = function () {
+                let data = JSON.parse(postLogin.response);
+                if (data.data && data.data.apiKey && data.data.guid) {
+                    chrome.storage.sync.set({ apiKey: data.data.apiKey, guid: data.data.guid }, function () {
+                        console.log("Credentials stored");
+                    });
+                    chrome.runtime.sendMessage({
+                        msg: "login-success",
+                    });
+                }
+                else {
+                    alert("Hmm. Seems like you do not have access yet. Please sign in to Chrome with an email that does or signup for access at https://flowdash.ai!")
+                }
+            }
+            postLogin.send(JSON.stringify({ userDetails: userDetails }));
+        };
+        x.send();
+    });
+}
+
 var publish = function () {
-    // TODO - Call API HERE.
-    let ownerId = 'testingGuid';
-    let apiKey = 'testingApiKey';
-    // let formData = new FormData();
-    // formData.append('data', JSON.stringify({
-    //     value: steps
-    // }));
+    chrome.storage.sync.get(['apiKey', 'guid'], function (data) {
+        console.log("Credentials found:", data);
+        if (data.apiKey && data.guid) {
+            let ownerId = data.guid;
+            let apiKey = data.apiKey;
 
-    var requestOptions = {
-        mode: "cors",
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            api_key: apiKey,
-        },
-    };
-    let fetchURL = new URL(CORE_API_BASE_URL + WALKTHROUGH_URL_PREFIX + `${ownerId}`);
-    var data = [];
-    // FIXME - Some issue with saving it as  steps.
-    for (let [key, value] of Object.entries(steps)) {
-        data = data.concat([{ screenId: key, steps: value }]);
-    }
-    console.log("Steps as of now:", data);
-    console.log({ data: JSON.stringify(data) })
-    fetchURL.search = new URLSearchParams({ data: JSON.stringify(data) }).toString();
+            var requestOptions = {
+                mode: "cors",
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    api_key: apiKey,
+                },
+            };
+            let fetchURL = new URL(CORE_API_BASE_URL + WALKTHROUGH_URL_PREFIX + `${ownerId}`);
+            var data = [];
+            for (let [key, value] of Object.entries(steps)) {
+                data = data.concat([{ screenId: key, steps: value }]);
+            }
+            console.log("Steps as of now:", data);
+            console.log({ data: JSON.stringify(data) })
+            fetchURL.search = new URLSearchParams({ data: JSON.stringify(data) }).toString();
 
-    fetch(fetchURL, requestOptions)
-        .then((response) => response)
-        .then((result) => {
-            console.log("Saved successfully.", result);
-        })
-        .catch((error) => {
-            console.log("error", error);
-        });
+            fetch(fetchURL, requestOptions)
+                .then((response) => response)
+                .then((result) => {
+                    console.log("Saved successfully.", result);
+                })
+                .catch((error) => {
+                    console.log("error", error);
+                });
+        }
+    });
+
 }
--- extension/popup.js
+++ extension/popup.js
@@ -36,15 +36,59 @@ function publish() {
         });
     });
 }
+function login() {
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        var activeTab = tabs[0];
+        chrome.runtime.sendMessage({
+            name: "login",
+            tabId: activeTab.id,
+        });
+    });
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("mode").addEventListener("click", popup);
     document.getElementById("replay").addEventListener("click", replay);
+    document.getElementById("signin").addEventListener("click", login);
     document.getElementById("publish").addEventListener("click", publish);
 });
 
-window.onload = function () {
-    chrome.storage.sync.get('mode', function (data) {
-        console.log("Capturing mode: ", data);
+function hideLogin(data) {
+    document.getElementById("mode").style.display = 'block';
+    document.getElementById("replay").style.display = 'block';
+    document.getElementById("publish").style.display = 'block';
+    document.getElementById("signin").style.display = 'none';
+    document.getElementById("clear").style.display = 'block';
+    document.getElementById("capture").style.display = 'flex'; 
+    if (data) {
         document.getElementById("mode").checked = data.mode;
+    }
+}
+function showLogin() {
+    document.getElementById("signin").style.display = 'block';
+    document.getElementById("mode").style.display = 'none';
+    document.getElementById("replay").style.display = 'none';
+    document.getElementById("publish").style.display = 'none';
+    document.getElementById("clear").style.display = 'none';
+    document.getElementById("capture").style.display = 'none'; 
+}
+
+window.onload = function () {
+    chrome.storage.sync.get(['mode', 'apiKey', 'guid'], function (data) {
+        console.log("Capturing mode: ", data);
+        if (data.apiKey && data.guid) {
+            hideLogin(data);
+        }
+        else {
+            showLogin(data);
+        }
     });
 }
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        if (request.msg === "login-success") {
+            hideLogin();
+        }
+    }
+);
